b160a2f91e38cafc69bc75f73d7a5ea22d8ca604
ivalan <ivchenkoalex@yahoo.com>
2016-07-21 17:50:21 +0300

    created project structure

---
+++ src/main/java/uits/jv1608/ivchenko/repositorytest/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uits.jv1608.ivchenko.repositorytest;
+
+/**
+ *
+ * @author Ya
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Howdy!");
+    }
+}
